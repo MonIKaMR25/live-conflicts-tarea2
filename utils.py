7cbb75805ec5b6ddb5054f7e0ff78319281a5d6e
Monica Munguia <monika_admi@hotmail.com>
2026-01-06 01:14:54 -0600

    Add util functions

---
+++ utils.py
@@ -0,0 +1,13 @@
+def suma(n1, n2):
+    return n1 + n2 
+
+def resta(n1, n2):
+    return n1 - n2
+
+def multiplica(n1, n2):
+    return n1 * n2
+
+def divide(n1, n2):
+    if n2 == 0:
+        return "Error: Division by zero"
+    return n1 / n2
